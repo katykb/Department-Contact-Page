4ae46a6bdc6079d71b81326a9379e29fc3cc8b36
Katy Brown <katybr35@gmail.com>
2022-05-11 18:13:23 -0500

    adding some css and reformatting template literal

--- index.js
+++ index.js
@@ -237,27 +237,28 @@ function addIntern() {
 }
 
 const addHTML = () => {
-  let employeeCard =""
-  let role
+  let employeeCard = "";
+  let role;
   for (let i = 0; i < myTeam.length; i++) {
     if (myTeam[i].getRole() === "Engineer") {
-      role = `<p class="card-text">role: ${myTeam[i].getRole()}.</p>
-     <p class="card-text">Github: ${myTeam[i].getGithub()}.</p>`;
+      role = `<p class="card-text">Role: ${myTeam[i].getRole()}.</p>
+      <p><a href="Github:${myTeam[i].getGithub()}" class="card-link">Github:</a><p>`;
     } else if (myTeam[i].getRole() === "Intern") {
-       role = `<p class="card-text">role: ${myTeam[i].getRole()}.</p>
+      role = `<p class="card-text">Role: ${myTeam[i].getRole()}.</p>
       <p class="card-text">School Name: ${myTeam[i].getSchool()}.</p>`;
     } else if (myTeam[i].getRole() === "Manager") {
-       role = `<p class="card-text">role: ${myTeam[i].getRole()}.</p>
+      role = `<p class="card-text">Role: ${myTeam[i].getRole()}.</p>
       <p class="card-text">Office Number: ${myTeam[i].getOffice()}.</p>`;
     }
-     employeeCard+= `<div class="card" style="width: 18rem;">
+    employeeCard += `<div class="row">
+    <div class="col-3">
+    <div class="card" style="width: 18rem;">
     <div class="card-body">
-      <h5 class="card-title">${myTeam[i].getName()}</h5>
+      <h5 class="card-title text-center">${myTeam[i].getName()}</h5>
       <h6 class="card-subtitle mb-2 text-muted">Id: ${myTeam[i].getId()}</h6>
-      ${role}      <a href="mailto:${myTeam[
-      i
-    ].getEmail()}" class="card-link">Email</a>
-      
+      ${role}      
+      <a href="mailto:${myTeam[i].getEmail()}" class="card-link">Email</a>
+      </div>
     </div>
   </div>`;
   }
@@ -270,11 +271,11 @@ const addHTML = () => {
   
       <!-- Bootstrap CSS -->
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  
+    <link rel=stylesheet href='./style.css'>
       <title>My Team</title>
     </head>
     <body>
-    <header class="bg-danger text-center text-white">
+    <header class="text-center text-white">
       <h1>My Team</h1>
       </header>
       <main class="container d-flex flex-wrap">
@@ -293,10 +294,10 @@ const addHTML = () => {
       -->
     </body>
   </html>`;
-  fs.writeFileSync('index.html', format, (error )=>{
-    if(error) throw error;
-  })
-  console.log(format)
+  fs.writeFileSync("index.html", format, (error) => {
+    if (error) throw error;
+  });
+  console.log(format);
 };
 
 createManager();
