1533f89969114c7dd49d7cb2e9371f063690f86f
Katy Brown <katybr35@gmail.com>
2022-05-11 15:56:54 -0500

    create template for html

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ const buildTeam = () => {
           addIntern();
           break;
 
-        // createManager();
+        default:
+          addHTML();
       }
     });
 };
@@ -104,56 +105,59 @@ const createManager = () => {
 };
 
 function addEngineer() {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Please enter the engineer's name",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          ("Enter a name to proceed.");
-        }
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the engineer's name",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            ("Enter a name to proceed.");
+          }
+        },
       },
-    },
-  
-    {
-      type: "input",
-      message: "Enter engineer's employee ID.",
-      name: "id",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Please enter ID to continue.";
-        }
+
+      {
+        type: "input",
+        message: "Enter engineer's employee ID.",
+        name: "id",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return "Please enter ID to continue.";
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      message: "Enter engineer's email. ",
-      name: "email",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Please enter engineer's email to continue.";
-        }
+      {
+        type: "input",
+        message: "Enter engineer's email. ",
+        name: "email",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return "Please enter engineer's email to continue.";
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      message: "Please enter the engineer's Github account",
-      name: "github",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Pleae enter github acocunt info to contine";
-        }
+      {
+        type: "input",
+        message: "Please enter the engineer's Github account",
+        name: "github",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return "Pleae enter github acocunt info to contine";
+          }
+        },
       },
-    }]).then((answers) => {
+    ])
+    .then((answers) => {
       console.log(answers);
       const engineer = new Engineer(
         answers.name,
@@ -232,6 +236,67 @@ function addIntern() {
     });
 }
 
-// };
+const addHTML = () => {
+  let employeeCard =""
+  let role
+  for (let i = 0; i < myTeam.length; i++) {
+    if (myTeam[i].getRole() === "Engineer") {
+      role = `<p class="card-text">role: ${myTeam[i].getRole()}.</p>
+     <p class="card-text">Github: ${myTeam[i].getGithub()}.</p>`;
+    } else if (myTeam[i].getRole() === "Intern") {
+       role = `<p class="card-text">role: ${myTeam[i].getRole()}.</p>
+      <p class="card-text">School Name: ${myTeam[i].getSchool()}.</p>`;
+    } else if (myTeam[i].getRole() === "Manager") {
+       role = `<p class="card-text">role: ${myTeam[i].getRole()}.</p>
+      <p class="card-text">Office Number: ${myTeam[i].getOffice()}.</p>`;
+    }
+     employeeCard+= `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${myTeam[i].getName()}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">Id: ${myTeam[i].getId()}</h6>
+      ${role}      <a href="mailto:${myTeam[
+      i
+    ].getEmail()}" class="card-link">Email</a>
+      
+    </div>
+  </div>`;
+  }
+  let format = `<!doctype html>
+  <html lang="en">
+    <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+      <!-- Bootstrap CSS -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  
+      <title>My Team</title>
+    </head>
+    <body>
+    <header class="bg-danger text-center text-white">
+      <h1>My Team</h1>
+      </header>
+      <main class="container d-flex flex-wrap">
+      ${employeeCard}
+      </main>
+  
+      <!-- Optional JavaScript; choose one of the two! -->
+  
+      <!-- Option 1: Bootstrap Bundle with Popper -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  
+      <!-- Option 2: Separate Popper and Bootstrap JS -->
+      <!--
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+      -->
+    </body>
+  </html>`;
+  fs.writeFileSync('index.html', format, (error )=>{
+    if(error) throw error;
+  })
+  console.log(format)
+};
 
 createManager();
